1fec557216d7417f8b70b6620c9ee5c56d6c54b8
KhaledAlhamoid <egur002277@gmail.com>
2025-06-09 22:29:55 +0200

    Update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'feed', loadComponent: () => import('./feed/feed').then(m => m.Feed) },
+  { path: 'user', loadComponent: () => import('./user/user').then(m => m.User) },
+  { path: 'upload', loadComponent: () => import('./upload/upload').then(m => m.Upload) },
+  { path: '', redirectTo: '/user' }
+];
